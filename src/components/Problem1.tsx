a6839916f884384e2c55245882727025cb295d11
rob2fresh4this <grijrob07@gmail.com>
2025-05-14 15:48:03 -0700

    Implement toggle button functionality in Problem1 component

--- src/components/Problem1.tsx
+++ src/components/Problem1.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Problem1 = () => {
+    // problem 1
+    // Toggle Button: Create a button that toggles between "Show" and "Hide" text when clicked.
+    const [isSwitch, setIsSwitch] = useState(false)
+
     return (
-        <div>Problem1</div>
+        <div className='flex flex-col gap-4'>
+            <div>Problem1</div>
+            <button onClick={() => setIsSwitch(!isSwitch)} className='bg-blue-500 text-white px-4 py-2 rounded'>
+                {isSwitch ? 'Hide' : 'Show'}
+            </button>
+        </div>
     )
 }
 
